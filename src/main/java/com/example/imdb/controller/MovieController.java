788a671a0346591d14bb0085f1e7f9f1eeeacde2
Milan <fasi.milan@gmail.com>
2020-08-16 15:41:47 +0200

    url string api keyword added

--- src/main/java/com/example/imdb/controller/MovieController.java
+++ src/main/java/com/example/imdb/controller/MovieController.java
@@ -3,11 +3,13 @@ package com.example.imdb.controller;
 import com.example.imdb.model.Movie;
 import com.example.imdb.service.MovieService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController(value = "/api")
+@RestController
+@RequestMapping("/api")
 public class MovieController {
 
     private final MovieService movieService;
