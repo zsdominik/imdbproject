a10a1e60d8e47fb5461bc6f68c09a20f7942b648
Milan <fasi.milan@gmail.com>
2020-08-16 16:07:38 +0200

    CrossOrigin avaiable

--- src/main/java/com/example/imdb/controller/MovieController.java
+++ src/main/java/com/example/imdb/controller/MovieController.java
@@ -2,12 +2,14 @@ package com.example.imdb.controller;
 
 import com.example.imdb.model.Movie;
 import com.example.imdb.service.MovieService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class MovieController {
